e786555eb398d7a6e933860106ef0c2136bf61f1
RoaaK95 <ruaakhuldoon.26@gmail.com>
2025-09-08 13:39:46 +0300

    ADD QUANITIFER COMPONENT: that handles the operations of increase and decrease or remove

--- src/components/Quantifier/Quantifier.tsx
+++ src/components/Quantifier/Quantifier.tsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import { type FunctionComponent, useState } from 'react'
 
-export const Quantifier = () => {
+import classes from './quantifier.module.scss'
+
+export type Operation = 'decrease' | 'increase'
+
+interface Props {
+  removeProductCallback: (productId: number) => void
+  handleUpdateQuantity: (productId: number, operation: Operation) => void
+  productId: number
+}
+
+
+export const Quantifier: FunctionComponent<Props> = ({ removeProductCallback, handleUpdateQuantity, productId }) => {
+  const [value, setValue] = useState<number>(1)
+
+  const reduce = ():void => {
+    handleUpdateQuantity(productId, 'decrease')
+
+    setValue(prevState => {
+      const updatedValue = prevState - 1
+      if (updatedValue === 0) {
+        removeProductCallback(productId)
+      }
+      return updatedValue
+    })
+  }
+
+  const increase = ():void => {
+    handleUpdateQuantity(productId, 'increase')
+    setValue(prevState => prevState + 1)
+  }
+
   return (
-    <div>Quantifier</div>
+    <div className={classes.quantifier}>
+      <input type="button" value="-" className={classes.buttonMinus} onClick={reduce} />
+      <input type="number"
+             step="1"
+             max=""
+             value={value}
+             onChange={e => setValue(parseInt(e.target.value))}
+             className={classes.quantityField} />
+      <input type="button" value="+" className={classes.buttonPlus} onClick={increase} />
+    </div>
   )
 }

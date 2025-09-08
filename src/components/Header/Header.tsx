df66d78a6b0ec0236ae18731c38a62e09b93d1e6
RoaaK95 <ruaakhuldoon.26@gmail.com>
2025-09-08 10:37:50 +0300

    ADD SHRINKHEADER FUNCTIONALITY TO HEADER : using DOM to manipulate the style of the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,50 @@
-import { type FunctionComponent } from "react";
+import { type FunctionComponent, useEffect } from "react";
 import {Link} from 'react-router-dom';
 import logo from "../../assets/logo.jpg";
+
 import classes from "./Header.module.scss"
 import { CartWidget } from '../CartWidget'
- 
+import {CartProps} from '../Products/Products,tsx'
+import useLocalStorageState from 'use-local-storage-state'
+
 
 export const Header: FunctionComponent =() =>{
+    useEffect(()=>{
+        window.addEventListener("scroll", () => shrinkHeader(), false)
+
+        return ()=>{
+            window.removeEventListener("scroll", () => shrinkHeader())
+        }
+    },[])
+
+    //shrink header using DOM 
+    const shrinkHeader = () => {
+    const DISTANCE_FROM_TOP = 140
+    const headerElement = document.querySelector("header") as HTMLElement
+    const logoElement = document.querySelectorAll("img")[0] as HTMLElement
+    const cartWidgetElement = document.querySelectorAll("img")[1] as HTMLElement
+    const productsCountElement = document.querySelector("span") as HTMLElement
+    const scrollY = document.body.scrollTop || document.documentElement.scrollTop
+
+    if (scrollY > DISTANCE_FROM_TOP) {
+      headerElement.style.transition = "height 200ms ease-in"
+      headerElement.style.height = "80px"
+      logoElement.style.transition = "height 200ms ease-in"
+      logoElement.style.height = "4rem"
+      cartWidgetElement.style.transition = "height 200ms ease-in"
+      cartWidgetElement.style.height = "2rem"
+      productsCountElement.style.transition = "font-size 200ms ease-in"
+      productsCountElement.style.fontSize = "1.5em"
+    } else {
+      headerElement.style.height = "150px"
+      logoElement.style.height = "6rem"
+      cartWidgetElement.style.height = "3rem"
+      productsCountElement.style.fontSize = "2em"
+    }
+  }
+
+  const [cart,] = useLocalStorageState<CartProps>('cart',{})
+  const productsCount: number = Object.keys(cart || {}).length
     return (
         <header>
             <div>
@@ -14,7 +53,7 @@ export const Header: FunctionComponent =() =>{
                 </Link>
             </div>
             <div>
-                <CartWidget productionCount={productsCount} />
+                <CartWidget productsCount={productsCount} />
             </div>
         </header>
     )

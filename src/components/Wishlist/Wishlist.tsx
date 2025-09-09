d342d39cd5c9a41f1ec14fbf4f2d7d6661dd0a22
RoaaK95 <ruaakhuldoon.26@gmail.com>
2025-09-09 12:58:08 +0300

    FIX: restyle the wishlist page

--- src/components/Wishlist/Wishlist.tsx
+++ src/components/Wishlist/Wishlist.tsx
@@ -11,23 +11,23 @@ interface WishlistProps {
 export const Wishlist: FunctionComponent<WishlistProps> = ({ wishlist, removeFromWishlist }) => {
   const wishlistItems = Object.values(wishlist || {});
 
-  if (wishlistItems.length === 0) {
-    return <div className={classes.empty}>Your wishlist is empty.</div>;
-  }
-
   return (
-    <section className={classes.wishlistPage}>
+    <section className={classes.wishlist}>
       <h1>Wishlist</h1>
-      <div className={classes.container}>
-        {wishlistItems.map(product => (
-          <div className={classes.product} key={product.id}>
-            <img src={product.thumbnail} alt={product.title} />
-            <h3>{product.title}</h3>
-            <p>Price: {product.price}</p>
-            <button onClick={() => removeFromWishlist(product.id)}>Remove</button>
-          </div>
-        ))}
-      </div>
+      {wishlistItems.length === 0 ? (
+        <div className={classes.empty}>Your wishlist is empty.</div>
+      ) : (
+        <div className={classes.container}>
+          {wishlistItems.map(product => (
+            <div className={classes.product} key={product.id}>
+              <img src={product.thumbnail} alt={product.title} />
+              <h3>{product.title}</h3>
+              <p>Price: {product.price}</p>
+              <button onClick={() => removeFromWishlist(product.id)}>Remove</button>
+            </div>
+          ))}
+        </div>
+      )}
     </section>
   );
 };

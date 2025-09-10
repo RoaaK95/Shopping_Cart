22d65e4eac3682eb9c37a1847b1fd55e120b7823
RoaaK95 <ruaakhuldoon.26@gmail.com>
2025-09-10 12:23:34 +0300

    ADD: products filter component that filters products based on categories and/or ratings

--- src/components/ProductsFilter/ProductsFilter.tsx
+++ src/components/ProductsFilter/ProductsFilter.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import {type FunctionComponent} from 'react'
 
-export const ProductsFilter = () => {
+
+type FilterProps ={
+  selectedCategory: string;
+  onCategoryChange: (category: string) => void
+  selectedRating: number | null
+  onRatingChange: (rating: number | null) =>void
+}
+
+const categories: string[] =['all', 'beauty', 'fragrances', 'groceries', 'furniture'];
+const stars: number[] =[1, 2, 3, 4 ,5];
+
+export const ProductsFilter: FunctionComponent<FilterProps> = ({ 
+  selectedCategory,
+  onCategoryChange,
+  selectedRating,
+  onRatingChange,
+}) => {
   return (
-    <div>ProductsFilter</div>
+    <div>
+      <select value={selectedCategory} onChange={e=> onCategoryChange(e.target.value)}>
+       {categories.map(cat =>(
+        <option key={cat} value={cat}>{cat.charAt(0).toUpperCase() + cat.slice(1)}</option>
+       ))}
+      </select>
+      
+      <select value={selectedRating ?? ''} onChange={e=> onRatingChange(Number(e.target.value) || null)}>
+      <option value="">All Ratings</option>
+      {stars.map(star=>(
+        <option key={star} value={star}>{star} Stars</option>
+      ))}
+      </select>
+    </div>
   )
 }

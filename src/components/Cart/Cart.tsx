e078d7bfd6f31bb6b1b462ede803b08d0e71fd25
RoaaK95 <ruaakhuldoon.26@gmail.com>
2025-09-15 10:27:50 +0300

    link the cart item to the product detailed page

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,66 +1,78 @@
-import { type FunctionComponent, useEffect } from 'react'
-import useLocalStorageState from 'use-local-storage-state'
+import { type FunctionComponent, useEffect } from "react";
+import useLocalStorageState from "use-local-storage-state";
 
-import { Quantifier } from '../Quantifier'
-import { type CartProps } from '../../types'
-import { TotalPrice } from '../TotalPrice'
-import { type Operation } from '../Quantifier/Quantifier.tsx'
-import classes from './cart.module.scss'
-import { useLocation } from 'react-router-dom'
-
+import { Quantifier } from "../Quantifier";
+import { type CartProps } from "../../types";
+import { TotalPrice } from "../TotalPrice";
+import { type Operation } from "../Quantifier/Quantifier.tsx";
+import classes from "./cart.module.scss";
+import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Cart: FunctionComponent = () => {
-  const [cart, setCart] = useLocalStorageState<CartProps>('cart', {})
-  const location = useLocation()
+  const [cart, setCart] = useLocalStorageState<CartProps>("cart", {});
+  const location = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [location])
+    window.scrollTo(0, 0);
+  }, [location]);
 
   const handleRemoveProduct = (productId: number): void => {
     setCart((prevCart) => {
-      const updatedCart = { ...prevCart }
-      delete updatedCart[productId]
-      return updatedCart
-    })
-  }
+      const updatedCart = { ...prevCart };
+      delete updatedCart[productId];
+      return updatedCart;
+    });
+  };
 
   const handleUpdateQuantity = (productId: number, operation: Operation) => {
     setCart((prevCart) => {
-      const updatedCart = { ...prevCart }
+      const updatedCart = { ...prevCart };
       if (updatedCart[productId]) {
-        if (operation === 'increase') {
-          updatedCart[productId] = { ...updatedCart[productId], quantity: updatedCart[productId].quantity + 1 }
+        if (operation === "increase") {
+          updatedCart[productId] = {
+            ...updatedCart[productId],
+            quantity: updatedCart[productId].quantity + 1,
+          };
         } else {
-          updatedCart[productId] = { ...updatedCart[productId], quantity: updatedCart[productId].quantity - 1 }
+          updatedCart[productId] = {
+            ...updatedCart[productId],
+            quantity: updatedCart[productId].quantity - 1,
+          };
         }
       }
-      return updatedCart
-    })
-  }
+      return updatedCart;
+    });
+  };
 
+  const getProducts = () => Object.values(cart || {});
 
-  const getProducts = () => Object.values(cart || {})
-
-  const totalPrice = getProducts().reduce((accumulator, product) => accumulator + (product.price * product.quantity), 0)
+  const totalPrice = getProducts().reduce(
+    (accumulator, product) => accumulator + product.price * product.quantity,
+    0
+  );
 
   return (
     <section className={classes.cart}>
       <h1>Cart</h1>
 
       <div className={classes.container}>
-        {getProducts().map(product => (
-          <div className={classes.product} key={product.id}>
-            <img src={product.thumbnail} alt={product.title} />
-            <h3>{product.title}</h3>
-            <Quantifier
-              removeProductCallback={() => handleRemoveProduct(product.id)}
-              productId={product.id}
-              handleUpdateQuantity={handleUpdateQuantity} />
-          </div>
+        {getProducts().map((product) => (
+          <Link to={`/product/${product.id}`}>
+            <div className={classes.product} key={product.id}>
+              <img src={product.thumbnail} alt={product.title} />
+              <h3>{product.title}</h3>
+
+              <Quantifier
+                removeProductCallback={() => handleRemoveProduct(product.id)}
+                productId={product.id}
+                handleUpdateQuantity={handleUpdateQuantity}
+              />
+            </div>
+          </Link>
         ))}
       </div>
       <TotalPrice amount={totalPrice} />
     </section>
-  )
-}
+  );
+};
